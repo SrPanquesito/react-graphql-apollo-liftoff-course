4358f57d447fa2616d20b1c05a8ba07c05cebe34
SrPanquesito <luisgdiazreynoso@gmail.com>
2022-12-15 22:19:26 -0800

    :sparkles: Liftoff V. Deplying to production.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://disgusting-battle-production.up.railway.app/',
   cache: new InMemoryCache(),
 })
 
